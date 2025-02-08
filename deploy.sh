1052ede7574add5752a679a894b2e578ee71e230
mihai.ciorobitca <mihai.ciorobitca@outlook.com>
2025-02-08 23:47:16 +0200

    m

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 git add .
 git commit -m "m"
 git push origin master
-docker build -t mihaidockerimage/reserve-now .
-docker push mihaidockerimage/reserve-now:latest
+sudo docker build -t mihaidockerimage/reserve-now .
+sudo docker push mihaidockerimage/reserve-now:latest
