44cdfb737c43d2a99bb9b99b5e440934f2c5e2e0
mihai.ciorobitca <mihai.ciorobitca@outlook.com>
2025-02-11 18:14:56 +0200

    m

--- blueprints/__init__.py
+++ blueprints/__init__.py
@@ -20,7 +20,7 @@ def create_app():
     app.register_blueprint(errors_blueprint)
     app.register_blueprint(admin_blueprint)
     app.register_blueprint(auth_blueprint, url_prefix="/auth")
-    app.register_blueprint(main_blueprint)
+    app.register_blueprint(main_blueprint, url_prefix="/home")
     app.register_blueprint(home_blueprint)
     
     return app
